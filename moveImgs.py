a965d7a01b6d45875183b1ee71dddff664edebd0
CameroJI <137007386+CameroJI@users.noreply.github.com>
2024-08-20 13:45:01 -0600

    Update moveImgs.py

--- moveImgs.py
+++ moveImgs.py
@@ -1,12 +1,11 @@
-import os
-from os import listdir
-from os.path import join, basename
-import shutil
+from os import listdir, makedirs
+from os.path import join, basename, dirname
+from shutil import copy, move
 import argparse
 import sys
 
 def moveImages(archivo_txt, carpeta_destino):
-    os.makedirs(carpeta_destino, exist_ok=True)
+    makedirs(carpeta_destino, exist_ok=True)
 
     with open(archivo_txt, 'r') as file:
         for linea in file:
@@ -14,25 +13,29 @@ def moveImages(archivo_txt, carpeta_destino):
             
             ruta_imagen = linea.split('-')[0]
             
-            subcarpeta = os.path.basename(os.path.dirname(ruta_imagen))
+            subcarpeta = basename(dirname(ruta_imagen))
             
-            nombre_imagen = os.path.basename(ruta_imagen)
+            nombre_imagen = basename(ruta_imagen)
             
             nuevo_nombre = f"{subcarpeta}-{nombre_imagen}"
             
             try:
-                shutil.copy(ruta_imagen, os.path.join(carpeta_destino, nuevo_nombre))
+                copy(ruta_imagen, join(carpeta_destino, nuevo_nombre))
                 print(f"Imagen copiada y renombrada: {nuevo_nombre}")
             except FileNotFoundError:
                 print(f"Error al copiar: {ruta_imagen}")
+    
+    txtFileNew = join(dirname(dirname(archivo_txt)), basename(archivo_txt))
+    move(archivo_txt, txtFileNew)
+    
                 
 def txtGetFiles(path):
     txtList = [join(path, file) for file in listdir(path) if file.endswith('.txt')]
     
     for txt in txtList:
-        txtName  = join(path, basename(txt).split('.')[0])
+        txtName  = basename(txt).split('.')[0]
         print(txtName)
-        moveImages(txt, join(path, txtName))
+        moveImages(txt, join(dirname(path), txtName))
 
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
