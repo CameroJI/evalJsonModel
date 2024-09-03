2cf2bd6360e460fe02416990c29d1c6ca82bda11
CameroJI <137007386+CameroJI@users.noreply.github.com>
2024-09-03 17:50:14 -0600

    Update

--- readJson.py
+++ readJson.py
@@ -21,18 +21,13 @@ def flatten_json(json_obj):
     return flat_dict
 
 def json_to_dataframe(json_file):
-    # Leer el archivo JSON
     with open(json_file, 'r') as file:
         data = json.load(file)
     
-    # Verificar si el JSON es una lista o un objeto
     if isinstance(data, list):
-        # Si es una lista de diccionarios
-        # Aplanar cada diccionario y crear el DataFrame
         flattened_data = [flatten_json(item) for item in data]
         df = pd.DataFrame(flattened_data)
     else:
-        # Si es un solo objeto JSON
         flattened_data = flatten_json(data)
         df = pd.DataFrame([flattened_data])
     
